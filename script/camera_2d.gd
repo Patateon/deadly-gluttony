747ccc6650fb407b287aa3f53afd9d8f776f28df
LordNounours <thomascaro627@gmail.com>
2024-09-26 14:16:07 +0200

    test interpolatin lerp

--- script/camera_2d.gd
+++ script/camera_2d.gd
@@ -1,17 +1,17 @@
 extends Camera2D
 
 var distdeadzone = 100.
-# Référence au CharacterBody2D (par exemple, le joueur)
-  # Assurez-vous que le chemin vers le personnage est correct
+var lerp_speed: float = 5.0
 @onready var character = get_node("/root/World/Player")
 
 func _ready() -> void:
 	make_current()
 	
 func _process(_delta: float) -> void:
-	update_camera_position()
+	update_camera_position(_delta)
 	
-func update_camera_position():
-	var current_dist = character.get_global_position().distance_to( get_screen_center_position())
-	if (current_dist > distdeadzone):
-		self.position = character.get_global_position()
+func update_camera_position(delta: float):
+	var current_dist = character.get_global_position().distance_to(get_screen_center_position())
+	if current_dist > distdeadzone:
+		var target_position = character.get_global_position()
+		self.position = self.position.lerp(target_position, lerp_speed * delta)
