e0be4803a9131be765b82660b58f0c0fe7528c63
Patateon <maximebeldjilali@gmail.com>
2024-10-16 17:10:25 +0200

    ajout ressource weapons stats

---
+++ script/weapons_stats.gd
@@ -0,0 +1,11 @@
+extends  Resource
+
+@export var damage = [[]]
+
+@export var attack_speed = [[]]
+
+@export var area = [[]]
+
+@export var projectile_speed = [[]]
+
+@export var number_proj = [[]]
